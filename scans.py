eb749c9b28723738e4fd420f3aa458b6f49437c7
SeikaHirori <SeikaHirori@outlook.com>
2023-03-07 13:51:36 -0800

    lists now go into instance variables, and stems are PURE stems now

--- scans.py
+++ scans.py
@@ -1,5 +1,5 @@
 import os
-from pathlib import Path
+from pathlib import Path, PurePath, PurePosixPath
 
 class scan_for_media:
     def __init__(self) -> None:
@@ -23,25 +23,55 @@ class scan_for_media:
         
         for index, file in enumerate(directory_media.glob("**/*")):
             print(f"current index: {index}")
-            f_name: str = file.name
-            f_stem: str = file.stem
-            f_suffix:str = file.suffixes
+            f_full_name: str = file.name
 
-            print(f"Printing file's name with extensions: {f_name}")
+            f_suffixes:str = file.suffixes
+
+            f_stem:str = None
+            if len(f_suffixes) > 1: 
+                f_stem = self.obtain_pure_stem(f_suffixes=f_suffixes, file=file)
+            else:
+                f_stem = file.stem
+
+
+            print(f"Printing file's name with extensions: {f_full_name}")
 
             print(f"Printing file's stem: {f_stem}")
             
-            print(f"Print suffixes: {f_suffix}")
+            print(f"Print suffixes: {f_suffixes}")
             
             # TODO - obtain ONLY numbers from file's string
             f_numbers: str = "WIP"
             print(f"Printing only numbers: {f_numbers}")
             print()
+
+            self.add_to_lists(f_full_name=f_full_name, f_stem=f_stem, f_suffixes=f_suffixes, f_numbers=f_numbers)
     
-    def add_to_lists(self, f_name: str, f_stem: str, f_suffix: str, f_numbers:str):
-        
+    def add_to_lists(self, f_full_name: str, f_stem: str, f_suffixes: str, f_numbers:str):
+        self.medias_with_extensions.append(f_full_name)
+        self.medias_only_stem.append(f_stem)
+        self.medias_suffixes.append(f_suffixes)
+        self.medias_only_numbers.append(f_numbers)
 
         print("FINISH ME!")
+
+    def obtain_pure_stem(self, f_suffixes:list[str],file:Path) -> str:
+        """ This ensures that the stem is PURE if there's more than one suffix.
+
+        Args:
+            f_suffixes (list[str]): _description_
+            file (Path): _description_
+
+        Returns:
+            str: _description_
+        """
+        output:Path = file.stem
+
+        index = 0
+        while index < len(f_suffixes):
+            output = PurePosixPath(output).stem
+            index += 1
+        return output
 
     def debug_print_lists(self):
         print(f"Media with extensions: {self.medias_with_extensions}")
