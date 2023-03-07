ea5502e8e1f6bdab72dbbc22c7b8aacf2aa9dfcf
SeikaHirori <SeikaHirori@outlook.com>
2023-03-07 00:09:25 -0800

    init

---
+++ main.py
@@ -0,0 +1,8 @@
+### BACKGROUND:
+    # This is for checking if all media files of iOS's Photos App exported to SD card
+
+# create an array that ranges from 0000 to most recent number of media file
+
+# Scan for all files in folder
+
+# 
