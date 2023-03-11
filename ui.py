9161a08277938645bb6b6d243c4df2deb853bd7e
SeikaHirori <SeikaHirori@outlook.com>
2023-03-10 16:45:44 -0800

    removed comments

--- ui.py
+++ ui.py
@@ -27,27 +27,20 @@ def main():
     scanned_goods:scan_for_media = scan_for_media()
 
     # Database objects
-    
-
     db_scanned:Scanned = Scanned()
-
     db_original_img: Original_Image = Original_Image()
     db_duplicate_img: Duplicate_Image = Duplicate_Image()
     db_misc_file: Misc_file = Misc_file()
     db_missing_IMG: Missing_Images = Missing_Images()
-    # print(f"Spreadsheet type: {db_duplicate_img.show_spreadsheet_type()}")
     
     scanned_goods.find_media(db_scanned=db_scanned)
-    # scanned_goods.debug_print_lists()
 
     desired_range:list[str] = logic_magic.createRange(starting_point=starting_point, end = ending_point)
 
     db_range:Selected_Range = Selected_Range()
     db_range.add_to_database(desired_range)
-    # db_range.debug_print_all_lists()
 
     db_scanned.import_list_of_dict(scanned_goods.export_list_of_dict())
-    # db_scanned.debug_print_all_lists()
 
     sorting_goods: Sorting_items = Sorting_items()
 
